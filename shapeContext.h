20c3354b4547f6b2557c0f0b2e655f1387bdede4
FuZihao <zihao_fu@163.com>
2014-10-19 23:05:16 +0800

    把算法封装成类的模式

--- shapeContext.h
+++ shapeContext.h
@@ -3,8 +3,36 @@
 
 #include <iostream>
 #include <opencv2/core/core.hpp>
-
+/*
+void add_to_tree(int x, int prevx);
 void update_labels();
 cv::Mat point_assignment_for_sc(const cv::Mat& cost_matrix);
+*/
+
+class hungarian_problem {
+public:
+	hungarian_problem();
+	hungarian_problem(const cv::Mat& cost_mat, int mode);
+	~hungarian_problem();
+	
+	void solve();
+	void print_cost_matrix();
+	void print_assigment_matrix();
+	void print_status();
+
+private:
+	cv::Mat cost_matrix;
+	vector<int> assignment_array;
+
+	vector<int> xy, yx;
+	vector<int> lx, ly;
+	vector<bool> S, T;
+	vector<int> slack, slackx;
+	vector<int> prev;
+	const int max_match;
+
+	void update_labels();
+	void add_to_tree(int x, int prevx);
+};
 
 #endif
